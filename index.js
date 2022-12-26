abf63ba4c05143d743c2ce4b0555e910d15515e4
Néstor <65639410+nesc11@users.noreply.github.com>
2022-12-26 07:37:01 -0500

    Build the app

--- index.js
+++ index.js
@@ -28,3 +28,44 @@ const posts = [
     }
 ]
 
+
+const postsEl = document.getElementById("posts-el")
+
+function showPost(posts) {
+    let postInfo = ""
+    for (let i = 0; i < posts.length; i++) {
+        let post = posts[i]
+        postInfo += `
+        <div class="container post-container">
+            <div class="user-info">
+                <img class="user" src="${post.avatar}" alt="">
+                <p class="username-p bold-text font-13">${post.name}<span class="location regular-text">${post.location}</span></p>
+            </div>
+            <img class="post-img" src="${post.post}" alt="">
+
+            <div class="post-footer">
+                <div class="post-icons">
+                    <img id="like-btn" src="images/icon-heart.png" alt="">
+                    <img src="images/icon-comment.png" alt="">
+                    <img src="images/icon-dm.png" alt="">
+                </div>
+                <p class="likes bold-text font-13"><span id="like-number" class="likes bold-text font-13">${post.likes}</span> likes</p>
+                <p class="comment regular-text font-13"><span class="bold-text">${post.username}</span> ${post.comment}</p>
+            </div>
+        </div>`
+    }
+    postsEl.innerHTML = postInfo
+}
+
+showPost(posts)
+
+const postContainers = Array.from(postsEl.getElementsByClassName("post-container"))
+
+postContainers.forEach(function(element) {
+    const likeBtn = element.querySelector("#like-btn")
+    const likeNumber = element.querySelector("#like-number")
+    likeBtn.addEventListener("click", function() {
+        likeNumber.textContent = Number(likeNumber.textContent) + 1
+    })
+})
+
